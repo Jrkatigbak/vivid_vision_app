496a9054026ea538c1f1ef006053e90992238bc5
Jrkatigbak <jrey.katigbak1996@gmail.com>
2024-09-25 07:14:07 +0800

    Adding login and register page

--- index.php
+++ index.php
@@ -151,16 +151,6 @@
 	<script src="assets/js/core/popper.min.js"></script>
 	<script src="assets/js/core/bootstrap.min.js"></script>
 
-	<!-- jQuery UI -->
-	<script src="assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
-	<script src="assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js"></script>
-
-	<!-- jQuery Scrollbar -->
-	<script src="assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
-
-	<!-- Bootstrap Notify -->
-	<script src="assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script>
-
 	<!-- Sweet Alert -->
 	<script src="assets/js/plugin/sweetalert/sweetalert.min.js"></script>
 
---
+++ register.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<title>Vivid Vision Outline</title>
+	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+	<link rel="icon" href="assets/img/icon.ico" type="image/x-icon"/>
+
+	<!-- Fonts and icons -->
+	<script src="assets/js/plugin/webfont/webfont.min.js"></script>
+	<script>
+		WebFont.load({
+			google: {"families":["Lato:300,400,700,900"]},
+			custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"], urls: ['assets/css/fonts.min.css']},
+			active: function() {
+				sessionStorage.fonts = true;
+			}
+		});
+	</script>
+
+	<!-- CSS Files -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/atlantis.min.css">
+	<link rel="stylesheet" href="assets/css/login.css">
+</head>
+<style>
+
+</style>
+<body class="login">
+    <div class="wrapper wrapper-login wrapper-login-full p-0">
+		<div class="login-aside w-50 d-flex flex-column align-items-center justify-content-center text-center bg text-white">
+			<h1 class="title fw-bold mb-3">Vivid Vision Outline</h1>
+			<p class="subtitle op-7">@2024 Vivid Vision App. All Rights Reserved.</p>
+		</div>
+		<div class="login-aside w-50 d-flex align-items-center justify-content-center bg-white">
+            <form method="POST" action="roguin/roguin">
+                <div class="container container-login container-transparent animated fadeIn">
+                    <div class="text-center">
+                    <img src="public/assets/img/ercg-logo-mark.svg" style="width:35%;" class="w-full mb-3 center" alt="">
+                    </div>
+                    <h3 class="text-center">Sign In</h3>
+
+                    <div class="login-form">
+                        <div class="form-group">
+                            <label for="logo" class="placeholder"><b>Company Logo</b></label>
+                            <input type="file" name="logo" class="form-control"  required>
+                        </div>
+                        <div class="form-group">
+                            <label for="company name" class="placeholder"><b>Company Name</b></label>
+                            <input type="text" name="company_name" class="form-control" placeholder="Company Name" required>
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="username" class="placeholder"><b>Email</b></label>
+                            <input type="text" name="email" class="form-control" placeholder="Email" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="password" class="placeholder"><b>Password</b></label>
+                            <div class="position-relative">
+                            <input type="password" name="password" class="form-control" placeholder="Password" required>
+                                <div class="show-password">
+                                    <i class="icon-eye"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group form-action-d-flex">
+                            <button type="submit"  class="btn btn-primary btn-block btn-block float-right mt-sm-0 fw-bold">Sign In</button>
+                        </div>
+                    </div>
+
+                </div>
+
+             
+            </form>
+		</div>
+	</div>
+	<!--   Core JS Files   -->
+	<script src="assets/js/core/jquery.3.2.1.min.js"></script>
+	<script src="assets/js/core/popper.min.js"></script>
+	<script src="assets/js/core/bootstrap.min.js"></script>
+	<!-- Sweet Alert -->
+	<script src="assets/js/plugin/sweetalert/sweetalert.min.js"></script>
+	<!-- Atlantis JS -->
+	<script src="assets/js/atlantis.min.js"></script>
+
+</body>
+</html>
